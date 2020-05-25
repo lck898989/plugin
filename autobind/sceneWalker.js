56bb0a4f98940354b58e340cc3cda89d2fdca340
李成昆 <lichengkun.lck8989@bytedance.com>
2020-05-25 20:40:15 +0800

    修改环节中加入进度条重复的bug

--- autobind/sceneWalker.js
+++ autobind/sceneWalker.js
@@ -55,199 +55,20 @@ module.exports = {
                 })
             })
         }
-        const refresh = async (dbUrl,res,resolve) => {
+        const refresh = async (dbUrl,res) => {
             // 刷新资源
             await new Promise((resolveIn,rejectIn) => {
                 Editor.assetdb.saveExists(dbUrl,res.serialize(),() => {
                     resolveIn();
                 })
             });
-            Editor.assetdb.refresh(dbUrl,() => {
-                resolve();
-            });
-        }
-
-        const addToPrefab = async (isTopic) => {
-
-        }
-        /**
-         *  定义内部使用的方法 
-         * 只针对非过渡关卡，以后优化
-         * @param  {String} dbUrl 预制体资源路径
-         * @param  {[cc.Prefab]} templates 预制体数组
-         * @param  {String} uuid 改资源的uuid
-         * @param  {String} comName 需要的脚本组件名字
-         * @param  {cc.Node} hornNode? 希望挂载的喇叭节点
-         * @param  {cc.Node} realNode? 希望挂载的实时教具节点
-         * @param  {cc.Node} progressNode? 希望挂载的进度条节点
-         * @param  {cc.Node} progressLevel? 当前进度条的第几个关卡
-         * @param  {Boolean} isChange? 是否应该在游戏环节关卡更换进度条的显示
-         */
-        const loadPrefab = (dbUrl,uuid,templates,comName,hornNode,realNode,progressNode,curLevelNum,progressLevel,isChange) => {
-
-            // 加载prefab资源
-            return new Promise((resolve,reject) => {
-                cc.loader.load({uuid},async (err,res) => {
-                    if(err) {
-                        reject();
-                        return;
-                    }
-                    // let resNode = cc.instantiate(res);
-                    if(templates.indexOf(res) < 0) {
-                        
-                        templates.push(res);
-                        
-                    }
-                    
-                    if(res) {
-                        res.data.active = true;
-
-                        let isRealTopic = comName && comName !== "emptyTemplateController" && comName !== "clickTemplateController";
-                        /** 进度条适配方法 */
-                        const progressAdapter = async () => {
-                            /*** 检查是否添加进度条暂停节点 */
-                            let sectionProgressNodeUuid = await getUuidByUrl(`db://assets/prefabs/progressBar_${curLevelNum}_${progressLevel}_stop.prefab`);
-                            if(sectionProgressNodeUuid === "null") {
-
-                                Editor.error(`请先添加progressBar_${curLevelNum}_${progressLevel}_stop.prefab预制体`);
-
-                            } else {
-                                /** 存在进度条暂停节点就把原来旧的进度条节点删除替换成新的 */
-                                let sectionProgressNode = await loadprefabNodeByUuid(sectionProgressNodeUuid);
-                                /** 检查该节点下是否有更换前的进度条节点 */
-                                progress = res.data.getChildByName(`progressBar_${curLevelNum}`);
-                                if(progress) {
-                                    /** 删除旧的进度条节点 */
-                                    progress.setParent(null);
-                                }
-
-                                /** 将该进度条节点添加到该预制体上 */
-                                sectionProgressNode.setParent(res.data);
-
-                            }
-                        }
-
-                        if(isRealTopic) {
-
-                            res.data.children.filter((node) => {
-                                /** 可交互节点上有动画组件并且动画的剪辑数量不为0 */
-                                if(node.name.indexOf("interactive") >= 0 && node.getComponent(cc.Animation) && node.getComponent(cc.Animation).getClips().length > 0) {
-                                    /** 挂载cc_play组件 */
-                                    if(!node.getComponent("cc_play")) {
-                                        node.addComponent("cc_play");
-                                        node.getComponent("cc_play").isRemote = true;
-                                    } else {
-                                        node.getComponent("cc_play").isRemote = true;
-                                    }
-                                
-                                }
-                            });
-                            /** 喇叭节点 */
-                            voiceNode = res.data.getChildByName("bt_voice");
-                            if(voiceNode && isDelete) {
-                                voiceNode.setParent(null);
-                            }
-                            /*** 实时教具节点 */
-                            realTimeNode = res.data.getChildByName("realTimeTeachingAids");
-                            if(realTimeNode && params.isDeleteReal) {
-                                realTimeNode.setParent(null);
-                            }
-
-                            
-                            /** 进度条节点 */
-                            if(!isChange) {
-                                progress = res.data.getChildByName(`progressBar_${curLevelNum}`);
-                                if(progress && params.isDeleteProgress) {
-                                    progress.setParent(null);
-                                }
-
-                            } else if(isChange && isChangeShowInSection) {
-
-                                await progressAdapter();
-
-                            }
-
-                            /** 设置进度条的状态 */
-                            if(progress) {
-                                progress.getComponent("cc_play_progress_bar").current = progressLevel;
-                            }
-                            
-                            // Editor.log("hornNode is ",hornNode);
-                            // Editor.log("realNode is ",realNode);
-
-                            if(hornNode && !voiceNode) {
-                                hornNode.setParent(res.data);
-                                
-                            }
-                            if(realNode && !realTimeNode) {
-                                realNode.setParent(res.data);
-                                
-                            }
-                            if(progressNode && !progress && !isChange) {
-
-                                progressNode.setParent(res.data);
-                                progressNode.getComponent("cc_play_progress_bar").current = progressLevel;
-                                    
-                            } else if(isChangeShowInSection && isChange) {
-                                // 用环节关卡特有的进度条
-                                await progressAdapter();
-                            }
-                            // 刷新资源
-                            await new Promise((resolveIn,rejectIn) => {
-                                Editor.assetdb.saveExists(dbUrl,res.serialize(),() => {
-                                    resolveIn();
-                                })
-                            });
-                            Editor.assetdb.refresh(dbUrl,() => {
-                                resolve();
-                            });
-                        }
-                        if(comName === "emptyTemplateController" || comName === "clickTemplateController") {
-                            /** 取消过渡场景交互节点的cc_play组件和默认动画的关闭 */
-                            res.data.children.filter((node) => {
-                                if(node.name.indexOf("interactive") >= 0 && node.getComponent(cc.Animation)) {
-                                    /** 删除可交互节点 */
-                                    node.setParent(null);
-                                    
-                                }
-                            });
-                            // 刷新资源
-                            await new Promise((resolveIn,rejectIn) => {
-                                Editor.assetdb.saveExists(dbUrl,res.serialize(),() => {
-                                    resolveIn();
-                                })
-                            });
-                            Editor.assetdb.refresh(dbUrl,() => {
-                                Editor.log("资源刷新完成");
-                                resolve();
-                            });
-                        }
-                        if(comName && !res.data.getComponent(comName)) {
-                            
-                            res.data.addComponent(comName);
-                            if(hornNode && realNode) {
-                                
-                            }
-
-                            // 刷新资源
-                            await new Promise((resolveIn,rejectIn) => {
-                                Editor.assetdb.saveExists(dbUrl,res.serialize(),() => {
-                                    resolveIn();
-                                })
-                            });
-                            Editor.assetdb.refresh(dbUrl,() => {
-                                Editor.log("资源刷新完成");
-                                resolve();
-                            });
-                        } else {
-                            resolve();
-                        } 
-                    }
+            await new Promise((res,rej) => {
+                Editor.assetdb.refresh(dbUrl,() => {
+                    res();
                 });
             })
         }
 
-        
         /** 测试环境配置文件相关信息 */
         let devConfigPath = Editor.Project.path + "/config_dev.json";
 
@@ -268,7 +89,7 @@ module.exports = {
                 return
             }
         }
-        Editor.log("config is ",config);
+        Editor.log("************************开始绑定脚本*****************************");
         let levels = JSON.parse(config).levels;
 
         let canIn = (levels && levels.length > 0 && params.templateCount === levels.length);
@@ -348,7 +169,7 @@ module.exports = {
 
                     // 将uuid添加到mainScene里面
                     if(hornNode && realNode) {
-                        await loadPrefab(dbUrl,uuid,templates,comName,hornNode,realNode,progressBarNode,curLevelNum,realLevelIndex,isChage);
+                        await loadPrefab(dbUrl,uuid,comName,isChage);
                     }
                     // 刷新编辑器
                     Editor.Ipc.sendToMain("autobind:saveScene");
@@ -358,11 +179,156 @@ module.exports = {
                     let tempPrefabUrl = `db://assets/templates/correspondTemplate1.prefab`;
                     let tempUuid = await getUuidByUrl(tempPrefabUrl);
                     if(tempUuid !== "null") {
-                        await loadPrefab(tempPrefabUrl,tempUuid,templates);
+                        await loadPrefab(tempPrefabUrl,tempUuid);
                     }
                 }
             }
 
+            /**
+             *  定义内部使用的方法 
+             * 只针对非过渡关卡，以后优化
+             * @param  {String} dbUrl 预制体资源路径
+             * @param  {String} uuid 改资源的uuid
+             * @param  {String} comName 需要的脚本组件名字
+             * @param  {Boolean} isChange? 是否应该在游戏环节关卡更换进度条的显示
+             */
+            const loadPrefab = async (dbUrl,uuid,comName,isChange) => {
+                
+                let voiceNode,progress,realTimeNode;
+                // 加载prefab资源
+                let res = await new Promise((resolve,reject) => {
+                    cc.loader.load({uuid},(err,res) => {
+                        if(err) {
+                            reject();
+                            return;
+                        }
+                        // let resNode = cc.instantiate(res);
+                        if(templates.indexOf(res) < 0) {
+                            
+                            templates.push(res);
+                            
+                        }
+                        resolve(res);
+                    });
+                });
+                if(res) {
+                    res.data.active = true;
+
+                    let isRealTopic = comName && comName !== "emptyTemplateController" && comName !== "clickTemplateController";
+                    /** 进度条适配方法 */
+                    const progressAdapter = async () => {
+                        /*** 检查是否添加进度条暂停节点 */
+                        let sectionProgressNodeUuid = await getUuidByUrl(`db://assets/prefabs/progressBar_${curLevelNum}_${realLevelIndex}_stop.prefab`);
+                        if(sectionProgressNodeUuid === "null") {
+
+                            Editor.error(`请先添加progressBar_${curLevelNum}_${realLevelIndex}_stop.prefab预制体`);
+
+                        } else {
+                            /** 存在进度条暂停节点就把原来旧的进度条节点删除替换成新的 */
+                            let sectionProgressNode = await loadprefabNodeByUuid(sectionProgressNodeUuid);
+                            /** 检查该节点下是否有更换前的进度条节点 */
+                            progress = res.data.getChildByName(`progressBar_${curLevelNum}`);
+                            if(progress) {
+                                /** 删除旧的进度条节点 */
+                                progress.setParent(null);
+                            }
+                        
+                            if(!res.data.getChildByName(`progressBar_${curLevelNum}_${realLevelIndex}_stop`)) {
+                                /** 将该进度条节点添加到该预制体上 */
+                                sectionProgressNode.setParent(res.data);
+                            }
+
+                        }
+                    }
+
+                    if(isRealTopic) {
+
+                        res.data.children.filter((node) => {
+                            /** 可交互节点上有动画组件并且动画的剪辑数量不为0 */
+                            if(node.name.indexOf("interactive") >= 0 && node.getComponent(cc.Animation) && node.getComponent(cc.Animation).getClips().length > 0) {
+                                /** 挂载cc_play组件 */
+                                if(!node.getComponent("cc_play")) {
+                                    node.addComponent("cc_play");
+                                    node.getComponent("cc_play").isRemote = true;
+                                } else {
+                                    node.getComponent("cc_play").isRemote = true;
+                                }
+                            
+                            }
+                        });
+                        /** 喇叭节点 */
+                        voiceNode = res.data.getChildByName("bt_voice");
+                        if(voiceNode && isDelete) {
+                            voiceNode.setParent(null);
+                        }
+                        /*** 实时教具节点 */
+                        realTimeNode = res.data.getChildByName("realTimeTeachingAids");
+                        if(realTimeNode && params.isDeleteReal) {
+                            realTimeNode.setParent(null);
+                        }
+
+                        
+                        /** 进度条节点 */
+                        if(!isChange) {
+                            progress = res.data.getChildByName(`progressBar_${curLevelNum}`);
+                            if(progress && params.isDeleteProgress) {
+                                progress.setParent(null);
+                            }
+
+                        } else if(isChange && isChangeShowInSection) {
+
+                            await progressAdapter();
+
+                        }
+
+                        /** 设置进度条的状态 */
+                        if(progress) {
+                            progress.getComponent("cc_play_progress_bar").current = realLevelIndex;
+                        }
+                        
+                        // Editor.log("hornNode is ",hornNode);
+                        // Editor.log("realNode is ",realNode);
+
+                        if(hornNode && !voiceNode) {
+                            hornNode.setParent(res.data);
+                            
+                        }
+                        if(realNode && !realTimeNode) {
+                            realNode.setParent(res.data);
+                            
+                        }
+                        /** 进度条节点资源有且当前情况下没有找到进度条节点或者是该转换进度条显示了且面板中没有勾选是否替换进度条   */
+                        if((progressBarNode && !progress && !isChange)) {
+
+                            progressBarNode.setParent(res.data);
+                            progressBarNode.getComponent("cc_play_progress_bar").current = realLevelIndex;
+                                
+                        } 
+                        if(isChangeShowInSection && isChange) {
+                            // 用环节关卡特有的进度条
+                            await progressAdapter();
+                        }
+                        await refresh(dbUrl,res);
+                    }
+                    if(comName === "emptyTemplateController" || comName === "clickTemplateController") {
+                        /** 取消过渡场景交互节点的cc_play组件和默认动画的关闭 */
+                        res.data.children.filter((node) => {
+                            if(node.name.indexOf("interactive") >= 0 && node.getComponent(cc.Animation)) {
+                                /** 删除可交互节点 */
+                                node.setParent(null);
+                                
+                            }
+                        });
+                        await refresh(dbUrl,res);
+                    }
+                    if(comName && !res.data.getComponent(comName)) {
+                        
+                        res.data.addComponent(comName);
+
+                        await refresh(dbUrl,res);
+                    }
+                }
+            }
             /** 向预制体数组中添加数组 */
             for(let i = 0; i < params.templateCount; i++) {
                 // 加载预制体
@@ -413,7 +379,7 @@ module.exports = {
 
                                 if(uuid !== "null") {
                                     isFindTransition = true;
-                                    prefabName.indexOf("dt") >= 0 ? await loadPrefab(dbUrl,uuid,templates,"emptyTemplateController") : await loadPrefab(dbUrl,uuid,templates,"clickTemplateController");
+                                    prefabName.indexOf("dt") >= 0 ? await loadPrefab(dbUrl,uuid,"emptyTemplateController") : await loadPrefab(dbUrl,uuid,"clickTemplateController");
                                     prefabName.indexOf("dt") >= 0 ? delays.push(2000) : delays.push(0);
                                 } else {
                                     isFindTransition = false;
@@ -434,7 +400,7 @@ module.exports = {
                                 } else {
                                     isFindTransition = true;
                                     /** 挂载相声脚本 */
-                                    await loadPrefab(dbUrl,uuid,templates,"emptyTemplateController");
+                                    await loadPrefab(dbUrl,uuid,"emptyTemplateController");
                                 }
                             } else if(prefabName === "") {
                                 Editor.error(`*********请检查第${i}个level中sections中的第${j}个环节配置中name属性是否存在**********`);
@@ -443,7 +409,7 @@ module.exports = {
                                 /* 如果转场类型的topic没有找到预制体的话先用btn_voice填个坑 */
                                 let tempPrefabUrl = `db://assets/templates/btn_voice.prefab`;
                                 if(hornUuid !== "null") {
-                                    await loadPrefab(tempPrefabUrl,hornUuid,templates);
+                                    await loadPrefab(tempPrefabUrl,hornUuid);
                                 }
                             }
                             
